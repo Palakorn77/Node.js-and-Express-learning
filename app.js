ab6f24d323f8cdec02ebe3ed62b9b805a9002504
hewteen <hewteen1994@gmail.com>
2022-11-17 16:53:48 +0700

    update html

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const express = require('express');
 const chalk = require('chalk')
 const debug = require('debug')('app')
 const morgan = require('morgan')
+const path = require('path')
 
 
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT; 
 
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get('/', (req,res) =>{
-    res.send('Hello Palakorn Phunual')
+    res.send('Hello Palakorn')
 })
 
-app.listen(port, ()=>{
-    debug('listening is on %d', chalk.green(port))
+app.listen(PORT, ()=>{
+    debug('listening is on %d', chalk.green(PORT))
 })
