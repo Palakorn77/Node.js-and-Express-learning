f068d59503fae15a0bdccb90f8ebeed1c6b7e4d5
hewteen <hewteen1994@gmail.com>
2022-11-23 14:20:50 +0700

    update djs

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-const express = require('express');
+const express = require('express')
 const chalk = require('chalk')
 const debug = require('debug')('app')
 const morgan = require('morgan')
 const path = require('path')
+const products = require('./data/products.json')
+const productRouter = express.Router()
 
 
-const app = express();
-const PORT = process.env.PORT; 
+const app = express()
+const PORT = process.env.PORT
 
 app.use(morgan('combined'))
-app.use(express.static(path.join(__dirname,"/public/")))
+app.use(express.static(path.join(__dirname, "/public/")))
 
-app.get('/', (req,res) =>{
-    res.send('Hello Palakorn')
+app.set("views", "./src/views")
+app.set("views engine", 'ejs')
+
+productRouter.route("/").get((req, res) => {
+    res.render("products.ejs",
+        products,
+    )
 })
 
-app.listen(PORT, ()=>{
-    debug('listening is on %d', chalk.green(PORT))
+productRouter.route("/:id").get((req, res) => {
+    const id = req.params.id
+    res.send("Hello Product is " +id)
+})
+
+app.use("/products", productRouter)
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+app.listen(PORT, () => {
+    debug('listening is on ' + chalk.green(PORT))
 })
